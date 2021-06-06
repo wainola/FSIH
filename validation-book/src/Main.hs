98006a71908167d1390cc8bbdd6ec178c8039244
Nicolas Riquelme <nrriquelme@gmail.com>
2021-06-06 01:28:45 -0400

    using either monad

--- validation-book/src/Main.hs
+++ validation-book/src/Main.hs
@@ -1,40 +1,61 @@
 module Main where
 import Data.Char
 
-checkPasswordLength :: String -> Maybe String
+checkPasswordLength :: String -> Either String String
 checkPasswordLength password =
   case (length password < 10) || (length password > 20) of
-    True -> Nothing
-    False -> Just password
+    True -> Left "Your password cannot be longer \
+    \ than 20 characters"
+    False -> Right password
 
-requireAlphaNum :: String -> Maybe String
+requireAlphaNum :: String -> Either String String
 requireAlphaNum xs =
   case (all isAlphaNum xs) of
-    False -> Nothing
-    True -> Just xs
+    False -> Left "Your password need alpha numeric characters"
+    True -> Right xs
 
-cleanWhitespace :: String -> Maybe String
-cleanWhitespace "" = Nothing
+cleanWhitespace :: String -> Either String String
+cleanWhitespace "" = Left "No a valid passwors"
 cleanWhitespace (x : xs) =
   case (isSpace x) of
     True -> cleanWhitespace xs
-    False -> Just(x : xs)
+    False -> Right (x : xs)
 
-checkPassword :: String -> String
-checkPassword password =
-  case (cleanWhitespace password) of
-    Nothing -> "Empty password are not valid ones"
-    Just password -> case (checkPasswordLength password) of
-      Nothing -> "The length of your password must be between 10 and 20 characters"
-      Just password -> case (requireAlphaNum password) of
-        Nothing -> "Your password require letters and numbers"
-        Just password -> "Valid password"
+-- checkPassword :: String -> String
+-- checkPassword password =
+--   case (cleanWhitespace password) of
+--     Nothing -> "Empty password are not valid ones"
+--     Just password -> case (checkPasswordLength password) of
+--       Nothing -> "The length of your password must be between 10 and 20 characters"
+--       Just password -> case (requireAlphaNum password) of
+--         Nothing -> "Your password require letters and numbers"
+--         Just password -> "Valid password"
 
-validatePassword :: String -> Maybe String
+validatePassword :: String -> Either String String
 validatePassword password =
   cleanWhitespace password
   >>= requireAlphaNum
   >>= checkPasswordLength
+
+
+printTestResult :: Either String () -> IO ()
+printTestResult r =
+  case r of
+    Left err -> putStrLn err
+    Right () -> putStrLn "All test passed."
+
+  
+eq :: (Eq a, Show a) => Int -> a -> a -> Either String ()
+eq n actual expected =
+  case (actual == expected) of
+    True -> Right ()
+    False -> Left (unlines [ "Test " ++ show n, " Expected: " ++ show expected, " But got: " ++ show actual])
+
+test :: IO()
+test = printTestResult $
+  do
+    eq 1 (checkPasswordLength "somePassword") (Right "somePassword")
+    eq 2 (checkPasswordLength "wainolaLoveBooks") (Right "wainolaLoveBooks")
 
 main :: IO ()
 main = do
